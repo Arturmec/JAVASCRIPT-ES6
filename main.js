4b2881d46a0e9498c69feb43fce2e1b075cc0ca6
unknown <artur0311@hotmail.com>
2019-11-11 21:18:37 -0200

    JS-ES6: Programas com conceitos como import, export e * as

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-const arr = [1,3,4,6,8];
+import * as funcoes from './funcoes';
 
-const newArr = arr.map (function(item){
-    return item*2;
-})
-
-console.log (newArr);
-
-const sum = arr.reduce (function(total, next){
-    return total+next;
-})
-console.log (sum);
-
-const filter = arr.filter(function(item){
-    return item % 2 === 0;
-})
-
-console.log (filter);
-
-const find = arr.find(function(item){
-    return item === 4;
-})
-
-console.log (find);
+console.log(funcoes);
